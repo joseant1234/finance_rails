d533348cbdb1cfce30f4a72aa5464d5a11bc1729
Jose Tafur <jose.tafur@tektonlabs.com>
2018-08-31 09:33:30 -0500

    fix error message when user try to upload image

--- app/models/income.rb
+++ app/models/income.rb
@@ -12,8 +12,7 @@ class Income < ApplicationRecord
   enum source: [:invoice, :direct]
 
   validates :description, :amount, :billing_at, :expiration_at, presence: true
-  validates :invoice_copy, :purchase_order, :invoice_number,
-            :purchase_order_number, presence: true, if: :invoice?
+  validates :invoice_copy, :invoice_number, presence: true, if: :invoice?
 
   has_attached_file :invoice_copy, default_url: "/images/default.png"
   validates_attachment_content_type :invoice_copy, content_type: /\Aimage\/.*\z/
